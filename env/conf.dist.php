2afe33ecead75933ae007c971ec477a18f264a97
Viacheslav Poturaev <vearutop@gmail.com>
2015-12-08 11:41:18 +0700

    conf

--- env/conf.dist.php
+++ env/conf.dist.php
@@ -6,5 +6,5 @@ use Yaoi\Database;
 use Yaoi\Log;
 
 Database::register('sqlite:///' . __DIR__ . '/../xh-stat.sqlite');
-Database::register('mysqli://root:@localhost/xhprof_stat2');
+//Database::register('mysqli://root:@localhost/xhprof_stat2');
 //Database::getInstance()->log(new Log('colored-stdout'));
